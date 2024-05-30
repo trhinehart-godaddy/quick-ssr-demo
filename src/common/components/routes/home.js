f2ecc86b4fea34d6b0ef37cf6ca93e412d715c83
trhinehart-godaddy <trhinehart@godaddy.com>
2024-05-30 08:53:14 -0700

    SSR linaria support

--- src/common/components/routes/home.js
+++ src/common/components/routes/home.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { css } from 'linaria';
+
+const styles = {
+  head: css`
+    background-color: cyan;
+  `
+}
 
 export default function () {
   return (
-    <h3>Home Page</h3>
+    <h3 className={ styles.head }>Home Page</h3>
   );
 }
